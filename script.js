664f19535af81a7056bfa3f303703365ddef84a2
Ferel775 <ferelrotikan703@gmail.com>
2026-04-08 19:50:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function readManga() {
+  alert("Jembut kecelup santen");
+}
+
+function toggleMenu() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("active");
+}
