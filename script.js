b3f4b0970ed00bb746fd68c4de52bcdaf8cb1310
Ferel775 <ferelrotikan703@gmail.com>
2026-04-08 20:34:05 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("active");
-}
-
-function readManga() {
-  alert("Jembut kecelup santen");
-}
